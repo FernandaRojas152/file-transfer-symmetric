b7ad3dfa94a1476428cb8ccb588161b3babdce69
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2022-06-02 11:16:30 -0500

    generate key and start server created

--- src/model/Server.java
+++ src/model/Server.java
@@ -4,6 +4,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.Key;
+
+import javax.crypto.spec.SecretKeySpec;
 
 public class Server{
 	private int port;
@@ -16,18 +19,17 @@ public class Server{
 	private String clientSHA;
 	
 	public Server() {
-		// TODO Auto-generated constructor stub
+		port= 9090;
+		serverSHA= "";
 	}
 	
 	public static void main(String[] args) {
 		
 	}
 
-
 	public int getPort() {
 		return port;
 	}
-
 
 	public void setPort(int port) {
 		this.port = port;
@@ -78,8 +80,33 @@ public class Server{
 		return clientSHA;
 	}
 
-
 	public void setClientSHA(String clientSHA) {
 		this.clientSHA = clientSHA;
+	}
+	
+	public void start() {
+		try {
+			serverSocket= new ServerSocket(port);
+			clientSocket= serverSocket.accept();
+			
+			out= new DataOutputStream(clientSocket.getOutputStream());
+			in = new DataInputStream(new DataInputStream(clientSocket.getInputStream()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Key generateKey(byte[] key){
+		byte[] byteKey= new byte[16];
+		for (int i = 0; i < 16; i++) {
+			byteKey[i]= key[i];
+		}
+		try {
+			Key keyAES= new SecretKeySpec(byteKey, "AES");
+			return keyAES;
+		} catch (Exception e) {
+			System.out.println("Error while generating key" + e);	
+		}
+		return null;
 	}
 }
