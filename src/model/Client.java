55b4822c16fe4c7815ef95c93bfeebdb4815ed79
Alex Samacá <alexadrito10@gmail.com>
2022-06-02 15:18:33 -0500

    Everything without connect

--- src/model/Client.java
+++ src/model/Client.java
@@ -5,19 +5,29 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.AlgorithmParameterGenerator;
+import java.security.AlgorithmParameters;
+import java.security.DigestInputStream;
 import java.security.Key;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidParameterSpecException;
 import java.util.Base64;
+import java.util.Random;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
-
-import org.apache.commons.codec.digest.DigestUtils;
+import javax.crypto.spec.DHParameterSpec;
 
 public class Client{
 	private String clientHost;
@@ -25,6 +35,7 @@ public class Client{
 	private String hash;
 	private DataOutputStream out;
 	private DataInputStream in;
+	private Key key;
 	public Client() {
 		clientHost="Fer";
 		port= 9090;
@@ -80,24 +91,30 @@ public class Client{
     }
 	
 	  public void generateSHA256(String parentDirectoryPath, String fileName) throws Exception {
+		  
+		  	out.writeUTF("cypher");
 	        String fullPath = parentDirectoryPath + "\\" + fileName;
 	        System.out.println(fullPath);
-	        //byte[] encryptedData = encryptFile(fullPath);
+	        String encryptedData = encryptFile(fullPath,key);
 	        //Se escriben los datos encripdatos localmente
 	        String encryptedFilePath = parentDirectoryPath + "\\" + "clientFileEncrypted.txt";
 	        File encryptedFile = new File(encryptedFilePath);
-	        //writeFile(encryptedFile, encryptedData);
+	        writeFile(encryptedFile, encryptedData.getBytes());
 	        //Se envia el nombre del archivo
 	        
-	        //sendMessage(fileName, out);
+	        sendMessage(fileName, out);
 	        
 	        //Se envia el archivo cifrado al servidor
-	        //sendFile(encryptedFile, out);
+	        sendFile(encryptedFile, out);
 	        
 	        
 	        //Se imprime el Sha1 del archivo no cifrado
 	        InputStream is = new FileInputStream(fullPath);
-	        String digest = DigestUtils.sha1Hex((is));
+	        //String digest = DigestUtils.sha1Hex((is));
+	        MessageDigest md = MessageDigest.getInstance("SHA-256");
+	        
+	        
+	        String digest = toHexString(md.digest(is.readAllBytes()));
 	        hash = digest;
 	        out.writeUTF(digest);
 	        System.out.println("full path: "+fullPath);
@@ -107,9 +124,9 @@ public class Client{
 	        out.writeUTF("exit");
 	    }
 
-	private String encryptFile(String fullPath, Key secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException {
+	private String encryptFile(String fullPath, Key secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException, IOException {
 		Cipher cipher= Cipher.getInstance("AES");	
-		byte[] textByte= fullPath.getBytes();
+		byte[] textByte= Files.readAllBytes(Paths.get(fullPath));
 		String encryptedFile= "";
 		
 		try {
@@ -123,4 +140,99 @@ public class Client{
 		
 		return encryptedFile;
 	}
+	   public static String toHexString(byte[] hash)
+	    {
+	        // Convert byte array into signum representation
+	        BigInteger number = new BigInteger(1, hash);
+	 
+	        // Convert message digest into hex value
+	        StringBuilder hexString = new StringBuilder(number.toString(16));
+	 
+	        // Pad with leading zeros
+	        while (hexString.length() < 64)
+	        {
+	            hexString.insert(0, '0');
+	        }
+	 
+	        return hexString.toString();
+	    }
+
+	   
+	   public void initializeDiffieHelman() throws NoSuchAlgorithmException, InvalidParameterSpecException, IOException {
+		   
+		   	out.writeUTF("df");
+			 AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
+	         paramGen.init(1024, new SecureRandom());
+	         AlgorithmParameters params = paramGen.generateParameters();
+	         DHParameterSpec dhSpec = (DHParameterSpec)params.getParameterSpec(DHParameterSpec.class);
+	         
+	         
+
+	            Random randomGenerator = new Random();
+
+	            BigInteger a = new BigInteger(1024, randomGenerator); // El secreto(privado)
+	            BigInteger p = dhSpec.getP(); //Numero primo(publico)
+	            BigInteger g = dhSpec.getG(); // Numero Primo generador de primos (publico)
+	            
+	            BigInteger A = g.modPow(a, p); // llave del cliente (A=g^a(modp)) (publico)
+	            
+	            //Mandar el numero primo
+	            out.writeUTF(p.toString());
+	            
+	            // mandar Primo generador de primos
+	            out.writeUTF(g.toString());
+	            
+	            // mandar calculo de A (llave cliente)
+	            out.writeUTF(A.toString());
+	            // Recibir la llave
+	            BigInteger B = new BigInteger(in.readUTF());
+	            
+	            // Calcular la llave secreta
+	            BigInteger encryptionKey = B.modPow(a, p);
+	            
+	            GenKeys gk = new GenKeys();
+	            key = gk.generateKey(encryptionKey.toByteArray());
+	            
+	            
+	            
+	            
+	            
+	      
+	   
+		 }
+	   
+	   public void sendFile(File file, DataOutputStream out) {
+		   try {
+		   
+		   FileInputStream fileIn = new FileInputStream(file);
+	        byte[] buf = new byte[Short.MAX_VALUE];
+	        int bytesRead;
+	        while ((bytesRead = fileIn.read(buf)) != -1) {
+	            out.writeShort(bytesRead);
+	            out.write(buf, 0, bytesRead);
+	        }
+	        out.writeShort(-1);
+	        fileIn.close();
+		   }catch (IOException i) {
+			   
+		   }
+		   
+		   
+		   
+	   }
+	    private void writeFile(File file, byte[] data) {
+	        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+	            outputStream.write(data);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	   
+	   public void sendMessage(String message, DataOutputStream out) throws IOException {
+	        out.writeUTF(message);
+	        out.flush();
+	    }
+	   
+	
+	
 }
--- src/model/Server.java
+++ src/model/Server.java
@@ -2,25 +2,48 @@ package model;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.security.AlgorithmParameterGenerator;
+import java.security.AlgorithmParameters;
 import java.security.Key;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Base64;
+import java.util.Objects;
+import java.util.Random;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.DHParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Server{
 	private int port;
-	private String hash;
 	private DataOutputStream out;
 	private DataInputStream in;
 	private ServerSocket serverSocket;
 	private Socket clientSocket;
 	private String serverSHA;
 	private String clientSHA;
+	private String targetFileName;
+	private String encryptedFilePath;
+	
+	
+	private Key serverKey;
+	
+	
+
+	
+	
 	
 	public Server() {
 		port= 9090;
@@ -35,9 +58,7 @@ public class Server{
 		return port;
 	}
 
-	public String getHash() {
-		return hash;
-	}
+
 
 
 	public ServerSocket getServerSocket() {
@@ -91,18 +112,129 @@ public class Server{
 	}
 	
 	public String decryptFile(String filePath, Key secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException {
+		
+      
+        
 		Cipher cipher= Cipher.getInstance("AES");
 		Base64.Decoder decoder= Base64.getDecoder();
 		byte[] encrypted= decoder.decode(filePath);
 		String decryptedFile="";
 		try {
+			
 			cipher.init(Cipher.DECRYPT_MODE, secretKey);
 			byte[] decrypted= cipher.doFinal(encrypted);
 			decryptedFile= new String(decrypted);
+			
+			  //String digest = DigestUtils.sha1Hex((is));
+	        MessageDigest md = MessageDigest.getInstance("SHA-256");
+	        
+	        
+	        String digest = toHexString(md.digest(decryptedFile.getBytes(StandardCharsets.UTF_8)));
+	        serverSHA = digest;
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		return decryptedFile;
 	}
+	  public static String toHexString(byte[] hash)
+	    {
+	        // Convert byte array into signum representation
+	        BigInteger number = new BigInteger(1, hash);
+	 
+	        // Convert message digest into hex value
+	        StringBuilder hexString = new StringBuilder(number.toString(16));
+	 
+	        // Pad with leading zeros
+	        while (hexString.length() < 64)
+	        {
+	            hexString.insert(0, '0');
+	        }
+	 
+	        return hexString.toString();
+	    }
+
+	 public int awaitCommand() throws IOException {
+	        int interrupt = -1;
+	        String command;
+	        try {
+	            command = in.readUTF();
+	            switch (Objects.requireNonNull(command)) {
+	                case "df" -> DiffieHelmanAnswer();                  
+	                case "cypher" -> {
+	                    targetFileName = in.readUTF();
+	                    
+	                    receiveFile(new File(encryptedFilePath), in);
+	                    decryptFile(encryptedFilePath,serverKey);
+	                    clientSHA = in.readUTF();
+	                }
+	                case "exit" -> {
+	                    clientSocket.close();
+	                    serverSocket.close();
+	                    interrupt = 1;
+	                    System.out.println("Connection terminated by client");
+	                }
+	                default -> {
+	                    out.writeUTF("Command not found");
+	                }
+	            }
+	            System.out.println("model.Client command: " + command);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return interrupt;
+	    }
+	 public void DiffieHelmanAnswer() throws IOException {
+		 //Recibe params del DiffieHelman
+		 BigInteger p = new BigInteger(in.readUTF());
+		 BigInteger g = new BigInteger(in.readUTF());
+		 BigInteger A =  new BigInteger(in.readUTF());
+		 
+		 //Generar el b secreto
+		 Random randomGenerator = new Random();
+         BigInteger b = new BigInteger(1024, randomGenerator);
+        //Calcular el B publico 
+         BigInteger B = g.modPow(b, p);
+         
+         //Mandar el B publico 
+         out.writeUTF(B.toString());
+         
+         
+      // Calcular la llave secreta
+         BigInteger decryptionKey = A.modPow(b, p);
+         //Generar llave AES
+         serverKey = generateKey(decryptionKey.toByteArray());
+          
+         
+         
+         
+         
+		 
+		 
+	 }
+	 
+	  public void receiveFile(File file, DataInputStream in) throws IOException {
+	        FileOutputStream fileOut = new FileOutputStream(file);
+	        byte[] buf = new byte[Short.MAX_VALUE];
+	        int bytesSent;
+	        while ((bytesSent = in.readShort()) != -1) {
+	            in.readFully(buf, 0, bytesSent);
+	            fileOut.write(buf, 0, bytesSent);
+	        }
+	        fileOut.close();
+	    }
+	  
+	  
+	  public boolean compareSHAS() {
+		  
+		  
+		  
+		  return serverSHA.equals(clientSHA);
+		  
+	  }
+
+	 
+	
 }
